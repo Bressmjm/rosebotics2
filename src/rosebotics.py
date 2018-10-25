e5046361d457a1dfb504d721b5c11afb0e3efdab
bressmjm <bressmjm@rose-hulman.edu>
2018-10-25 16:22:52 -0400

    Rosebotics

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -123,6 +123,10 @@ class DriveSystem(object):
         where positive is clockwise and negative is counter-clockwise),
         stopping by using the given StopAction.
         """
+        self.left_wheel.start_spinning(duty_cycle_percent)
+        self.right_wheel.start_spinning(duty_cycle_percent)
+        self.left_wheel.stop_moving(stop_action)
+        self.right_wheel.stop_spinning(stop_action)
         # TODO: Do a few experiments to determine the constant that converts
         # TODO:   from wheel-degrees-spun to robot-degrees-spun.
         # TODO:   Assume that the conversion is linear with respect to speed.
